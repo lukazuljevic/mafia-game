d94f1bdb41cc6345c287122ff5f3348423f3d7e9
lukazuljevic <luka.zuljo@gmail.com>
2025-12-24 16:17:44 +0100

    Fix error

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('*', (req, res, next) => {
+app.get('{*splat}', (req, res, next) => {
   if (req.path.startsWith('/socket.io')) return next();
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
